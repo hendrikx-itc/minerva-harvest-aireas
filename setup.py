873e5d962829c1d62c4d8aad7c67ec4410437b4b
Tommy van Schie <tommy.vanschie@hendrikx-itc.nl>
2019-04-04 15:41:10 +0200

    Added gitignore file

--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ __author__ = "Hendrikx ITC"
 
 setup(
     name="minerva-harvest-aireas",
+    description='Minerva harvest plugin for aireas json data',
     version="1.0.0",
     description=__doc__,
     author=__author__,
     author_email='info@hendrikx-itc.nl',
+    python_requires='>=3.5',
     install_requires=["minerva"],
     packages=["minerva_harvest_aireas"],
     package_dir={"": "src"},
